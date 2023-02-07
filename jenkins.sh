c05d313773ef038618d4258383d75cdedcd89ea2
sufiyan <khansufiyan779@gmail.com>
2023-02-07 16:35:03 +0530

    added

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-​
-#copying mvn settings.xml to /usr/share/maven/config
+
 sudo cp -rf ./settings.xml /usr/share/maven/conf/
 ​
-#copying tomcat-users.xml to /var/lib/tomcat9/conf
 sudo cp -rf ./tomcat-users.xml /var/lib/tomcat9/conf/
 
 sudo cp -r ~/remote_dir/workspace/onlinejava/target/*.war /var/lib/tomcat9/webapps/
